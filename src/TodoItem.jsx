80b83cdf8261d21a1d2975bda744e10739950c2b
ohsand <osand.business@gmail.com>
2023-04-17 13:44:17 +0200

    first commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TodoItem = ({completed, id, title, toggleTodo, deleteTodo}) => {
+  return (
+    <li>
+        <label>
+        <input type="checkbox" checked={completed} onChange={e => toggleTodo(id, e.target.checked)}/>
+        {title}
+        </label>
+        <button className='btn btn-danger' onClick={() => deleteTodo(id)}>Delete</button>
+    </li>
+  )
+}
+
+export default TodoItem
---
+++ src/TodoList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+const TodoList = ({todos, toggleTodo, deleteTodo}) => {
+  return (
+    <ul className='list'>
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+          return (
+            <TodoItem id={todo.id} completed={todo.completed} title={todo.title} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+          )
+        })}
+      </ul>
+  )
+}
+
+export default TodoList
